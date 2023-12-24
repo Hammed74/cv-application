0bf502dab06eafdf3071445ba8ab392b78c16dca
Rarri74 <Hammed74@gmail.com>
2023-12-24 15:48:05 -0500

    added open/close to personal details

--- src/FormComponents/PersonalDetails.jsx
+++ src/FormComponents/PersonalDetails.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react"
 
 export default function PersonalDetails({details, setDetails}){
+    const [open, setOpen] = useState(true)
     
     function handleSetDetails(e){
         const section = e.target.className
@@ -9,14 +11,47 @@ export default function PersonalDetails({details, setDetails}){
     }
 
     return (
-        <>
+      <>
         <div className="personal-details">
-            <h2>Personal Details</h2>
-            <input type="text" className="fullName" value={details.fullName} onChange={(e) => {handleSetDetails(e)}}/>
-            <input type="email" className="email" value={details.email} onChange={(e) => {handleSetDetails(e)}} />
-            <input type="text" className="phoneNumber" value={details.phoneNumber} onChange={(e) => {handleSetDetails(e)}} />
-            <input type="text" className="city" value={details.city} onChange={(e) => {handleSetDetails(e)}} />
+              <h2>Personal Details</h2>
+              <img onClick={() => setOpen(!open)} className="hamburger" src="../src/assets/hamburger.png" width="20px" alt="" />
+          {open ? (
+            <>
+              <input
+                type="text"
+                className="fullName"
+                value={details.fullName}
+                onChange={(e) => {
+                  handleSetDetails(e);
+                }}
+              />
+              <input
+                type="email"
+                className="email"
+                value={details.email}
+                onChange={(e) => {
+                  handleSetDetails(e);
+                }}
+              />
+              <input
+                type="text"
+                className="phoneNumber"
+                value={details.phoneNumber}
+                onChange={(e) => {
+                  handleSetDetails(e);
+                }}
+              />
+              <input
+                type="text"
+                className="city"
+                value={details.city}
+                onChange={(e) => {
+                  handleSetDetails(e);
+                }}
+              />
+            </>
+          ) : null}
         </div>
-        </>
-    )
+      </>
+    );
 }
